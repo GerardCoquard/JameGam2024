5b28de470b94aef9f37e6c32b5cc36ba5d0efbb8
Gerard Coquard <gerard.coquard.franco@gmail.com>
2024-07-23 03:00:33 +0200

    Grid Builder + Grid working

--- Assets/GridManager.cs
+++ Assets/GridManager.cs
@@ -1,38 +1,85 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteAlways]
 public class GridManager : MonoBehaviour
 {
-    private Grid _grid;
-    public GameObject fillPrefab;
-
+    public static GridManager instance;
+    
+    [SerializeField] private Grid _grid;
+    [SerializeField] private Transform _baseGroundHolder;
+    [SerializeField] private Transform _collider;
+    [SerializeField] private Transform _holders;
+    [SerializeField] private GameObject _groundPrefab;
+    private Dictionary<Tile, Transform> _holdersDictionary;
     public int gridWidth;
-    public int gridHeight;
-    // Start is called before the first frame update
-    void Start()
+    public int gridDepth;
+
+    private void Awake()
     {
-        _grid = GetComponent<Grid>();
-        FillGrid();
+        instance = this;
+        GetHolders();
     }
 
-    private void FillGrid()
+    public void FillGridWithGround()
     {
+        transform.position = new Vector3(-_grid.cellSize.x * gridWidth / 2, 0, -_grid.cellSize.z * gridDepth / 2);
+        
+        _collider.localPosition = new Vector3(_grid.cellSize.x * gridWidth / 2, -0.5f, _grid.cellSize.z * gridDepth / 2);
+        _collider.localScale = new Vector3(gridWidth, 1f, gridDepth);
+        
+        Transform[] grounds = _baseGroundHolder.GetComponentsInChildren<Transform>();
+        foreach (var t in grounds)
+        {
+            if(t == _baseGroundHolder) continue;
+            DestroyImmediate(t.gameObject);
+        }
+        
         for (int i = 0; i < gridWidth; i++)
         {
             int x = (int)_grid.cellSize.x * i;
-            for (int j = 0; j < gridHeight; j++)
+            for (int j = 0; j < gridDepth; j++)
             {
                 int z = (int)_grid.cellSize.z * j;
-                Vector3Int cellPos = _grid.WorldToCell(new Vector3(x, 0, z));
-                Instantiate(fillPrefab, _grid.GetCellCenterWorld(cellPos), Quaternion.identity);
+                Vector3Int cellPos = _grid.LocalToCell(new Vector3(x, 0, z));
+                Instantiate(_groundPrefab, _grid.GetCellCenterWorld(cellPos), Quaternion.identity, _baseGroundHolder);
             }
         }
     }
-    
-    public void Add(Vector3 pos, GameObject prefab)
+
+    public void Add(Vector3 pos, GameObject prefab, Tile tile)
     {
+        if (!_holdersDictionary.ContainsKey(tile))
+            CreateHolder(tile);
         Vector3Int cellPos = _grid.WorldToCell(pos);
-        Instantiate(prefab, _grid.GetCellCenterWorld(cellPos), Quaternion.identity);
+        Instantiate(prefab, _grid.GetCellCenterWorld(cellPos), Quaternion.identity, _holdersDictionary[tile]);
     }
+
+    private void CreateHolder(Tile tile)
+    {
+        GameObject holder = new GameObject(tile.ToString());
+        holder.transform.position = transform.position;
+        holder.transform.SetParent(_holders);
+        _holdersDictionary.Add(tile,holder.transform);
+    }
+
+    public void GetHolders()
+    {
+        _holdersDictionary = new Dictionary<Tile, Transform>();
+        for (int i = 0; i < _holders.childCount; i++)
+        {
+            GameObject h = _holders.GetChild(i).gameObject;
+            _holdersDictionary.Add((Tile) Enum.Parse(typeof(Tile), h.name, true),h.transform);
+        }
+    }
+}
+
+public enum Tile
+{
+    BaseGround,
+    Ground,
+    Wizard,
+    Decoration
 }
--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -1,42 +1,98 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using Object = UnityEngine.Object;
 
+[ExecuteAlways]
 public class InputManager : MonoBehaviour
 {
-    [SerializeField]
-    private Camera sceneCamera;
-
-    private Vector3 lastPosition;
-
-    [SerializeField]
-    private LayerMask placementLayermask;
-
-    public event Action OnClicked, OnExit;
+    public static InputManager instance;
+    
+    [SerializeField] private Camera _sceneCamera;
+    [SerializeField] private LayerMask _clickLayerMask;
+    public static event Action<Vector3,string> OnGridClick;
+    public static event Action OnExit;
+    public static event Action<Vector3> OnStartDrag;
+    public static event Action<Vector3> OnEndDrag;
+    private void Awake()
+    {
+        instance = this;
+    }
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-            OnClicked?.Invoke();
+        if (Input.GetMouseButtonDown(0))
+        {
+            CheckIfClickOnGrid();
+        }
         if(Input.GetKeyDown(KeyCode.Escape))
-            OnExit?.Invoke();
+            CancelAction();
     }
 
     public bool IsPointerOverUI()
         => EventSystem.current.IsPointerOverGameObject();
-
-    public Vector3 GetSelectedMapPosition()
+    
+    public bool ClickOnGridEditor(out string layer, out Vector3 pos, Vector3 mousePos, Camera cam)
     {
-        Vector3 mousePos = Input.mousePosition;
-        mousePos.z = sceneCamera.nearClipPlane;
-        Ray ray = sceneCamera.ScreenPointToRay(mousePos);
+        layer = "";
+        pos = Vector3.zero;
+        
+        mousePos.z = cam.nearClipPlane;
+        Ray ray = HandleUtility.GUIPointToWorldRay(mousePos);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 100, placementLayermask))
+        
+        if (Physics.Raycast(ray, out hit, 1000, _clickLayerMask))
         {
-            lastPosition = hit.point;
+            layer = LayerMask.LayerToName(hit.collider.gameObject.layer);
+            pos = hit.point;
+            return true;
         }
-        return lastPosition;
+        return false;
+    }
+
+    public bool ClickOnGrid(out string layer, out Vector3 pos)
+    {
+        layer = "";
+        pos = Vector3.zero;
+        
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = _sceneCamera.nearClipPlane;
+        Ray ray = _sceneCamera.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1000, _clickLayerMask))
+        {
+            layer = LayerMask.LayerToName(hit.collider.gameObject.layer);
+            pos = hit.point;
+            return true;
+        }
+        return false;
+    }
+
+    private void CheckIfClickOnGrid()
+    {
+        if (IsPointerOverUI()) return;
+        string layer;
+        Vector3 pos;
+        if(ClickOnGrid(out layer, out pos))
+            OnGridClick?.Invoke(pos,layer);
+    }
+
+    public void CancelAction()
+    {
+        OnExit?.Invoke();
+    }
+
+    public void Deselect()
+    {
+        StartCoroutine(DeselectOnFrameEnd());
+    }
+
+    IEnumerator DeselectOnFrameEnd()
+    {
+        yield return new WaitUntil(() => Selection.objects.Length > 0);
+        Selection.objects = Array.Empty<Object>();
     }
 }
