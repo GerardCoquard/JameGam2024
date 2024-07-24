b6759fcbc9d5a742ce2fbb72ec57742d119bc35f
RocFauconTuron <rfauconi@edu.tecnocampus.cat>
2024-07-25 00:02:37 +0200

    purified

--- Assets/Script/Towers/SlowEnemy.cs
+++ Assets/Script/Towers/SlowEnemy.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SlowEnemy : MonoBehaviour
 {
     public float slowMultiplier;
+    private Vector3 anteriorScale;
+    [SerializeField] Material purifiedMat;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +16,19 @@ public class SlowEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.localScale != anteriorScale)
+        {
+            anteriorScale = transform.localScale;
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, transform.localScale.x/2);
+            foreach (var hitCollider in hitColliders)
+            {
+                if(hitCollider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+                {
+                    hitCollider.gameObject.layer = LayerMask.NameToLayer("PurifiedGround");
+                    hitCollider.GetComponent<MeshRenderer>().material = purifiedMat;
+                }
+            }
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
