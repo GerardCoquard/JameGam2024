2de9bda9f666d7a62bda4787e77afc746ebcd6c4
Gerard Coquard <gerard.coquard.franco@gmail.com>
2024-07-25 00:57:33 +0200

    cositas

--- Assets/Script/GameData.cs
+++ Assets/Script/GameData.cs
@@ -8,6 +8,11 @@ public class GameData : ScriptableObject
     public List<TowerData> towers;
     public float attackSpeedMultiplier;
     public float rangeMultiplier;
+    public int upgradeBasePriceNormal;
+    public int upgradeBasePriceArmor;
+    public int upgradeBasePriceMagicArmor;
+    public int upgradeBasePriceFireRate;
+    public int upgradeBasePriceRange;
     public float priceMultiplier;
     public float normalMultiplier;
     public float armorMultiplier;
