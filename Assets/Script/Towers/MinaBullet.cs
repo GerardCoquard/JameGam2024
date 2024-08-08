a6cc0dbd0e001d71572a66c10f8e0d4b341a03d1
RocFauconTuron <rfauconi@edu.tecnocampus.cat>
2024-08-08 21:08:17 +0200

    MY EYES

--- Assets/Script/Towers/MinaBullet.cs
+++ Assets/Script/Towers/MinaBullet.cs
@@ -7,6 +7,7 @@ public class MinaBullet : Bullet
     private void OnTriggerEnter(Collider other)
     {
         other.transform.parent.GetComponent<Enemy>().DamageEnemy(tower, tower.damage);
+        tower.GetComponent<TowerMinas>().RemovePos(transform.position);
         Destroy(gameObject);
     }
 }
--- Assets/Script/Towers/TowerMinas.cs
+++ Assets/Script/Towers/TowerMinas.cs
@@ -3,35 +3,74 @@ using System.Collections.Generic;
 using UnityEngine;
 public class TowerMinas : Tower
 {
-    public int numberMinas;
     public LayerMask myLayerMask;
     List<RaycastHit> hits;
+    List<Vector3> bulletPos;
+    [SerializeField] float heightPath;
+    int indexBullet = 0;
     // Start is called before the first frame update
     public override void StartTower(TowerData data)
     {
+        bulletPos = new List<Vector3>();
         base.StartTower(data);
         GetSplinesInRange();
     }
+    public void RemovePos(Vector3 pos)
+    {
+        pos.y -= heightPath;
+        bulletPos.Remove(pos);
+        indexBullet = 0;
+    }
     public void GetSplinesInRange()
     {
+        indexBullet = 0;
         hits = new List<RaycastHit>(Physics.SphereCastAll(transform.position, range, transform.forward, 1f, myLayerMask));
-    }
 
+        for (int i = 0; i < hits.Count; i++)
+        {
+            if (Vector3.Distance(hits[i].transform.position, transform.position) > range)
+            {
+                hits.RemoveAt(i);
+                i--;
+            }
+        }
+    }
+    public override void AddLevelRange()
+    {
+        base.AddLevelRange();
+        GetSplinesInRange();
+    }
     // Update is called once per frame
     protected override void Update()
     {
         if (fireRateTimer < 1 / fireRate)
         {
             fireRateTimer += Time.deltaTime;
-        }
-            
+        }           
         else
         {
             fireRateTimer = 0;
-            if(hits.Count > 0)
+            if(hits.Count > bulletPos.Count)
             {
-                int randomPath = Random.Range(0, hits.Count);
-                GameObject bullet = GameObject.Instantiate(bulletPrefab, hits[randomPath].transform.position, Quaternion.identity);
+                Vector3 pos = hits[indexBullet].transform.position;
+                indexBullet++;
+                if (bulletPos.Contains(pos))
+                {
+                    bool hasFoundPos = false;
+                    while (!hasFoundPos)
+                    {
+                        pos = hits[indexBullet].transform.position;
+                        if (!bulletPos.Contains(pos))
+                        {
+                            hasFoundPos = true;
+                        }
+                        indexBullet++;
+                    }
+                }
+                bulletPos.Add(pos);
+                pos.y += heightPath;
+                GameObject bullet = GameObject.Instantiate(bulletPrefab, pos, Quaternion.identity);
+                bullet.GetComponent<Bullet>().Action(this, target);
             }
             
         }
